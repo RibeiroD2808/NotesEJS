e25e947963e1b541ad8ec5714f6bb59ef8d4b3e3
RibeiroD2808 <diogo.ribeiro.2808@gmail.com>
2023-12-21 19:29:35 +0000

    Get method finished

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+const names = ["POST1", "POST2"]; 
+
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {names:names});
+});
+
+app.post("/submit", (req, res) => {
+
+    console.log(req.body.post1);
+    res.render("index.ejs", {names:names});
 });
 
 app.listen(port, () => {
---
+++ public/scripts/script.js
@@ -0,0 +1,7 @@
+console.log(names.length);
+console.log(names[0]);
+
+for (let i = 0; i < names.length; i++) {
+    $("body").append("Some appended text.");
+    
+}
